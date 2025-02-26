175e2aee6f5f255e0b4415a06fdefeaa55593bcb
Kevin Ehmka <kevin.ehmka@capco.com>
2025-02-26 12:31:03 +0800

    streamstatus tests

--- test/bloc/stream_status_test.dart
+++ test/bloc/stream_status_test.dart
@@ -3,110 +3,95 @@ import 'package:juice/juice.dart';
 import '../test_helpers.dart';
 
 void main() {
-//   group('StreamStatus Tests', () {
-//     final state = TestState(value: 0);
-//     final newState = TestState(value: 1);
-//     final event = TestEvent();
+  group('StreamStatus Tests', () {
+    final state = TestState(value: 0);
+    final newState = TestState(value: 1);
+    final event = TestEvent();
 
-//     test('StreamStatus.updating creates correct status', () {
-//       final updating = StreamStatus<TestState>.updating(newState, state, event);
+    test('StreamStatus.updating creates correct status', () {
+      final updating = StreamStatus<TestState>.updating(newState, state, event);
 
-//       expect(updating.isUpdating, true);
-//       expect(updating.isWaiting, false);
-//       expect(updating.isFailure, false);
-//       expect(updating.isCanceling, false);
+      expect(updating.isWaitingFor(), false);
+      expect(updating.isCancelingFor(), false);
+      expect(updating.isFailureFor(), false);
+      expect(updating.isUpdatingFor(), true);
 
-//       expect(updating.state, newState);
-//       expect(updating.oldState, state);
-//       expect(updating.event, event);
-//     });
+      expect(updating.state, newState);
+      expect(updating.oldState, state);
+      expect(updating.event, event);
+    });
 
-//     test('StreamStatus.waiting creates correct status', () {
-//       final waiting = StreamStatus<TestState>.waiting(newState, state, event);
+    test('StreamStatus.waiting creates correct status', () {
+      final waiting = StreamStatus<TestState>.waiting(newState, state, event);
 
-//       expect(waiting.isUpdating, false);
-//       expect(waiting.isWaiting, true);
-//       expect(waiting.isFailure, false);
-//       expect(waiting.isCanceling, false);
+      expect(waiting.isWaitingFor(), true);
+      expect(waiting.isCancelingFor(), false);
+      expect(waiting.isFailureFor(), false);
+      expect(waiting.isUpdatingFor(), false);
 
-//       expect(waiting.state, newState);
-//       expect(waiting.oldState, state);
-//       expect(waiting.event, event);
-//     });
+      expect(waiting.state, newState);
+      expect(waiting.oldState, state);
+      expect(waiting.event, event);
+    });
 
-//     test('StreamStatus.failure creates correct status', () {
-//       final failure = StreamStatus<TestState>.failure(newState, state, event);
+    test('StreamStatus.failure creates correct status', () {
+      final failure = StreamStatus<TestState>.failure(newState, state, event);
 
-//       expect(failure.isUpdating, false);
-//       expect(failure.isWaiting, false);
-//       expect(failure.isFailure, true);
-//       expect(failure.isCanceling, false);
+      expect(failure.isUpdatingFor(), false);
+      expect(failure.isWaitingFor(), false);
+      expect(failure.isFailureFor(), true);
+      expect(failure.isCancelingFor(), false);
 
-//       expect(failure.state, newState);
-//       expect(failure.oldState, state);
-//       expect(failure.event, event);
-//     });
+      expect(failure.state, newState);
+      expect(failure.oldState, state);
+      expect(failure.event, event);
+    });
 
-//     test('StreamStatus.canceling creates correct status', () {
-//       final canceling =
-//           StreamStatus<TestState>.canceling(newState, state, event);
+    test('StreamStatus.canceling creates correct status', () {
+      final canceling =
+          StreamStatus<TestState>.canceling(newState, state, event);
 
-//       expect(canceling.isUpdating, false);
-//       expect(canceling.isWaiting, false);
-//       expect(canceling.isFailure, false);
-//       expect(canceling.isCanceling, true);
+      expect(canceling.isUpdatingFor(), false);
+      expect(canceling.isWaitingFor(), false);
+      expect(canceling.isFailureFor(), false);
+      expect(canceling.isCancelingFor(), true);
 
-//       expect(canceling.state, newState);
-//       expect(canceling.oldState, state);
-//       expect(canceling.event, event);
-//     });
+      expect(canceling.state, newState);
+      expect(canceling.oldState, state);
+      expect(canceling.event, event);
+    });
 
-//     test('StreamStatus.copyWith creates a copy with correct properties', () {
-//       final updating = StreamStatus<TestState>.updating(state, state, event);
-//       final copied = updating.copyWith(
-//         newState: newState,
-//         event: TestEvent(groups: {"new-group"}),
-//       );
+    test('StreamStatus.copyWith creates a copy with correct properties', () {
+      final updating = StreamStatus<TestState>.updating(state, state, event);
+      final copied = updating.copyWith(
+        state: newState,
+        oldState: state,
+        event: TestEvent(groups: {"new-group"}),
+      );
 
-//       expect(copied.isUpdating, true); // Status remains the same
-//       expect(copied.state, newState); // State updated
-//       expect(copied.event?.groupsToRebuild,
-//           contains("new-group")); // Event updated
-//     });
+      expect(copied.isUpdatingFor(), true); // Status remains the same
+      expect(copied.state, newState); // State updated
+      expect(copied.event?.groupsToRebuild,
+          contains("new-group")); // Event updated
+    });
 
-//     test('StreamStatus.copyWithStatus changes status', () {
-//       final updating = StreamStatus<TestState>.updating(state, state, event);
+    test('isStateTypeOf checks state type correctly', () {
+      final status = StreamStatus<TestState>.updating(state, state, event);
 
-//       final waiting = updating.copyWithStatus(StreamStatusType.waiting);
-//       expect(waiting.isWaiting, true);
+      expect(status is UpdatingStatus, true);
+      expect(status.state is SecondTestState, false);
+    });
 
-//       final failure = updating.copyWithStatus(StreamStatusType.failure);
-//       expect(failure.isFailure, true);
+    test('When status types match typesafe helper methods', () {
+      final waiting = StreamStatus<TestState>.waiting(state, state, event);
 
-//       final canceling = updating.copyWithStatus(StreamStatusType.canceling);
-//       expect(canceling.isCanceling, true);
+      expect(waiting.isWaitingFor<TestState>(), true);
+      expect(waiting.isWaitingFor<SecondTestState>(), false);
 
-//       final backToUpdating = waiting.copyWithStatus(StreamStatusType.updating);
-//       expect(backToUpdating.isUpdating, true);
-//     });
+      final failure = StreamStatus<TestState>.failure(state, state, event);
 
-//     test('isStateTypeOf checks state type correctly', () {
-//       final status = StreamStatus<TestState>.updating(state, state, event);
-
-//       expect(status.isStateTypeOf<TestState>(), true);
-//       expect(status.isStateTypeOf<SecondTestState>(), false);
-//     });
-
-//     test('When status types match typesafe helper methods', () {
-//       final waiting = StreamStatus<TestState>.waiting(state, state, event);
-
-//       expect(waiting.isWaitingFor<TestState>(), true);
-//       expect(waiting.isWaitingFor<SecondTestState>(), false);
-
-//       final failure = StreamStatus<TestState>.failure(state, state, event);
-
-//       expect(failure.isFailureFor<TestState>(), true);
-//       expect(failure.isFailureFor<SecondTestState>(), false);
-//     });
-//   });
+      expect(failure.isFailureFor<TestState>(), true);
+      expect(failure.isFailureFor<SecondTestState>(), false);
+    });
+  });
 }
